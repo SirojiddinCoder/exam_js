cf0d88ff4be5aa9e60bf74c3acc4c54e4ae2ac08
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-15 09:43:32 -0800

    test1

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
 // var ar = 'salom'
 // console.log(ar.padStart(8));
-	var ar = 'salom';
+	// var ar = 'salom';
     //  console.log(ar.replace(ar[0], ar[ar.length - 2]));
+
+    // console.log(0.1 + 0.2);
+    // console.log(0.1 + 0.2 == 0.3);
+    const person = { age: 20, name: 'Ben' };
+    const peopleArray = [person, person, person];
+    peopleArray[1].name = 'Joe';
+    console.log(peopleArray[0].name);
