8d3d267a5c1b0aea2ce55112fddfefcd7753b03f
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-15 09:45:56 -0800

    test2

--- js.js
+++ js.js
@@ -5,7 +5,19 @@
 
     // console.log(0.1 + 0.2);
     // console.log(0.1 + 0.2 == 0.3);
-    const person = { age: 20, name: 'Ben' };
-    const peopleArray = [person, person, person];
-    peopleArray[1].name = 'Joe';
-    console.log(peopleArray[0].name);
+    // const person = { age: 20, name: 'Ben' };
+    // const peopleArray = [person, person, person];
+    // peopleArray[1].name = 'Joe';
+    // console.log(peopleArray[0].name);
+
+//     const person = {  age: 20,  name: 'Ben'};
+// const peopleArray   =  [{...person}, {...person}, {...person}];
+// peopleArray[1].name = 'Joe';
+// console.log(peopleArray[0].name);
+
+// console.log(new Number(5) === 5);
+
+const myArr = [1,2,3];
+const arrTwo = myArr.splice(0,4).slice(0,2);
+arrTwo[0] = 9;
+console.log(arrTwo);
