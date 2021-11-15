5f50478d6470ee4f54ee953a9228a6e2586038e9
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-15 09:46:57 -0800

    test3

--- js.js
+++ js.js
@@ -17,7 +17,14 @@
 
 // console.log(new Number(5) === 5);
 
-const myArr = [1,2,3];
-const arrTwo = myArr.splice(0,4).slice(0,2);
-arrTwo[0] = 9;
-console.log(arrTwo);
+// const myArr = [1,2,3];
+// const arrTwo = myArr.splice(0,4).slice(0,2);
+// arrTwo[0] = 9;
+// console.log(arrTwo);
+
+// const arr1 = 'jimmy'.split('');
+// const arr2 = arr1.reverse();
+// const arr3 = arr2.splice(0,5);
+// const arr4 = arr3.slice(0,5);
+// const arrControl = 'jimmy'.split('').reverse().splice(0,5).slice(0,5);
+// console.log(arrControl)
